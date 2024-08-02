a9dac83ebeb6b392fa82000b4fa8a4fccad52edc
Andre White <adarro@gmail.com>
2024-08-01 18:28:11 -0600

    trunk fixes

--- build-logic/src/main/kotlin/djaxonomy.quality-sonar.gradle.kts
+++ build-logic/src/main/kotlin/djaxonomy.quality-sonar.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
-    id ("org.sonarqube")
+    id("org.sonarqube")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "myProjectKey")
+        property("sonar.host.url", "myHostUrl")
+    }
 }
--- subprojects/common/ddo-core/src/test/scala/io/truthencode/ddo/model/feats/SpecialFeatTest.scala
+++ subprojects/common/ddo-core/src/test/scala/io/truthencode/ddo/model/feats/SpecialFeatTest.scala
@@ -5,10 +5,10 @@ import io.truthencode.ddo.model.feats.SpecialFeat.FeatRespecToken
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
-class SpecialFeatTest extends AnyFunSpec with Matchers with LazyLogging  {
+class SpecialFeatTest extends AnyFunSpec with Matchers with LazyLogging {
   describe("A Feat Respec Token") {
     it("should be retrieved from the base Feat Search") {
-     Feat.values should contain(SpecialFeat.FeatRespecToken)
+      Feat.values should contain(SpecialFeat.FeatRespecToken)
     }
   }
 }
